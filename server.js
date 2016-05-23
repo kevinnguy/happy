ea9689bf0d6beef39b1a2e34aabc5ae435a8f8fd
Kevin Nguy <kevnguy@gmail.com>
2016-05-23 16:15:01 -0700

    Create task route to insert data in postgres

--- server.js
+++ server.js
@@ -2,11 +2,11 @@
 
 const Hapi = require('hapi');
 const Good = require('good');
-
+const PG = require('pg');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
-server.connection({ port: process.env.PORT || 8000 });
+server.connection({ port: process.env.PORT });
 
 // Add the route
 server.route({
@@ -23,6 +23,29 @@ server.route({
     path: '/{name}',
     handler: function (request, reply) {
         reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/task/{name}',
+    handler: function (request, reply) {
+      PG.connect(process.env.DATABASE_URL, function(err, client, done) {
+        // Handle connection errors
+        if(err) {
+          done();
+          console.log(err);
+        }
+
+        // SQL Query > Insert Data
+        client.query("INSERT INTO items(text, complete) values($1, $2)", [request.params.name, true]);
+
+        const query = client.query("SELECT * FROM items ORDER BY id ASC");
+        query.on('end', function() {
+            done();
+            reply('You\'ve created a task called: ' + encodeURIComponent(request.params.name));
+        });
+      });
     }
 });
 
