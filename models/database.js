cae13cdafbc21b63b23b6905035b9fc5ad0df522
Kevin Nguy <kevnguy@gmail.com>
2016-05-23 16:14:36 -0700

    Refactor database.js

--- models/database.js
+++ models/database.js
@@ -1,8 +1,7 @@
-var pg = require('pg');
-var connectionString = process.env.DATABASE_URL;
+const PG = require('pg');
 
-var client = new pg.Client(connectionString);
+const client = new pg.Client(process.env.DATABASE_URL);
 client.connect();
 
-var query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+const query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
 query.on('end', function() { client.end(); });
