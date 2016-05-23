0b619941afa005269f6c8712e7291bf5d9c40212
Kevin Nguy <kevnguy@gmail.com>
2016-05-23 16:32:36 -0700

    Fix database file

--- models/database.js
+++ models/database.js
@@ -1,6 +1,6 @@
 const PG = require('pg');
 
-const client = new pg.Client(process.env.DATABASE_URL);
+const client = new PG.Client(process.env.DATABASE_URL);
 client.connect();
 
 const query = client.query('CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
